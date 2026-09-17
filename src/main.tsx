36a77fc64e88514f4f00bce16e16970b2b4f69b9
Daramola Femi() <68006992+DaramolaFemi@users.noreply.github.com>
2026-09-17 03:53:08 +0100

    Load Bouldwood identity pass last

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/index.css";
 import "./styles/refinement.css";
+import "./styles/identity-pass.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
